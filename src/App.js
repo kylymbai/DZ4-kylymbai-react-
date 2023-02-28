e11af282d7b7cad686bf6bc7539f6f7b2e604880
Kylymbai <kylymbaialmazbekuulu@gmail.com>
2023-02-28 14:53:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class NewForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Отправленное имя: ' + this.state.value)
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className='login'>
+        <div className='avatar'>
+          <img className='avatar-img' src='https://goo-gl.me/FPUud' />
+        </div>
+        <h2>Login</h2>
+        <h3>Wellcome back</h3>
+        <form 
+            onSubmit={this.handleSubmit}
+            className='login-form'>
+          <div className='form-group'>
+            <input 
+              value={this.state.value}
+              onChange={this.handleChange}
+              type='email' 
+              placeholder='Username'/>
+          </div>
+          <div className='form-group'>
+            <input 
+              type='password'
+              placeholder='Password'/>
+          </div>
+          <button type='submit'>LOGIN</button>
+        </form>
+
+      </div>
+
+  
+    )
+  }
+
 }
 
-export default App;
+export default NewForm;
